e477b1c3a06804978895a5a4ffe294a6eefa8bea
Aldatreesa JOSY <aldatreesajosy05@gmail.com>
2026-06-12 18:45:19 +0530

    Updated backend URL to Render

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://campus-link-management-system-v260.vercel.app/api",
+  baseURL: "https://campuslink-backend-bgph.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
